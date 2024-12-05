2c41ad289c1621cf2eb79d0f661bcd0001464e37
WinterStay-hsr <winterstay972@gmail.com>
2024-12-05 21:32:19 +0800

    1.7

--- src/view/start/StartFrame.java
+++ src/view/start/StartFrame.java
@@ -3,5 +3,5 @@ package view.start;
 public class StartFrame {
     //todo: design a frame for the start of the game
     //todo: 'log in', 'sign up', 'guest mode', 'help', 'exit' buttons
-    //todo:1.6!
+    //todo:1.6!1.7abaabaa
 }
